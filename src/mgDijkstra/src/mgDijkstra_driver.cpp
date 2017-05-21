d487cf7c90633156755c25f401262323ffa17cd5
Maoguang Wang <xjtumg1007@gmail.com>
2017-05-22 02:31:16 +0800

    fix appvoyer

--- src/mgDijkstra/src/mgDijkstra_driver.cpp
+++ src/mgDijkstra/src/mgDijkstra_driver.cpp
@@ -27,6 +27,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 ********************************************************************PGR-GNU*/
 
+#include "./mgDijkstra_driver.h"
 
 #include <sstream>
 #include <deque>
