53f60e336df2a0ffb961d61e37094f262d5fe5ae
cvvergara <vicky@georepublic.de>
2020-12-26 17:15:59 -0600

    Boost1.55 adding multi_point

--- include/cpp_common/bline.hpp
+++ include/cpp_common/bline.hpp
@@ -28,6 +28,9 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #define INCLUDE_CPP_COMMON_BLINE_HPP_
 #pragma once
 
+#if BOOST_VERSION=1.54.0
+#include <boost/bgeometry/geometries/multi_point.hpp>
+#else
 #if BOOST_Geometry_VERSION_OK
 #include <boost/geometry/geometries/multi_point.hpp>
 #include <boost/geometry/geometries/linestring.hpp>
@@ -40,6 +43,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include <boost/bgeometry/geometries/multi_linestring.hpp>
 #include <boost/bgeometry/geometries/polygon.hpp>
 #include <boost/bgeometry/geometries/multi_polygon.hpp>
+#endif
 #endif
 
 namespace bg = boost::geometry;
