a1953b6fdee96e22bb983e1609307001716b1032
cvvergara <vicky@georepublic.de>
2020-12-27 14:48:24 -0600

    dealing with alpha shape

--- src/alpha_shape/alphaShape_driver.cpp
+++ src/alpha_shape/alphaShape_driver.cpp
@@ -38,8 +38,9 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include "cpp_common/pgr_base_graph.hpp"
 #include "cpp_common/bpoint.hpp"
 #include "cpp_common/bline.hpp"
+#if Boost_VERSION_MACRO > 105500
 #include <boost/geometry/io/wkt/write.hpp>
-#if 0
+#else
 #include <boost/bgeometry/io/wkt/write.hpp>
 #endif
 
