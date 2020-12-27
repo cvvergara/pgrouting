89191f86d4ad315d5419d89b87ee5254105da751
cvvergara <vicky@georepublic.de>
2020-12-27 13:32:36 -0600

    dealing with bline

--- include/cpp_common/bline.hpp
+++ include/cpp_common/bline.hpp
@@ -31,18 +31,18 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #if Boost_VERSION_MACRO > 105500
 #include <boost/geometry/geometries/multi_point.hpp>
 #include <boost/geometry/geometries/multi_linestring.hpp>
+#include <boost/geometry/geometries/multi_polygon.hpp>
 #else
 #include <boost/bgeometry/geometries/multi_point.hpp>
 #include <boost/bgeometry/geometries/multi_linestring.hpp>
+#include <boost/bgeometry/geometries/multi_polygon.hpp>
 #endif
 
 #include <boost/geometry/geometries/linestring.hpp>
 #include <boost/geometry/geometries/polygon.hpp>
-#include <boost/geometry/geometries/multi_polygon.hpp>
 #if 0
 #include <boost/bgeometry/geometries/linestring.hpp>
 #include <boost/bgeometry/geometries/polygon.hpp>
-#include <boost/bgeometry/geometries/multi_polygon.hpp>
 #endif
 
 namespace bg = boost::geometry;
