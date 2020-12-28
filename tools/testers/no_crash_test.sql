9ad01b6228d21adb349b402958f9ec55a7502923
Vicky Vergara <vicky@georepublic.de>
2020-12-28 11:51:30 -0600

    more on testing conditional pgtap

--- tools/testers/no_crash_test.sql
+++ tools/testers/no_crash_test.sql
@@ -26,18 +26,20 @@ BEGIN
 
         -- RAISE WARNING '%', q1;
 
-
-        RETURN query SELECT * FROM lives_ok(q1);
-        IF i = 0 THEN
-            RETURN query SELECT * FROM isnt_empty(q1);
+        IF _pgr_versionless((SELECT boost from pgr_full_version()), '1.54.0') AND func='pgr_alphashape' THEN
+            RETURN QUERY SELECT * FROM  skip('pgr_alphaSahpe not supported when compiled with Boost version < 1.54.0', 2);
         ELSE
-            IF func='pgr_alphashape' THEN
+            RETURN query SELECT * FROM lives_ok(q1);
+            IF i = 0 THEN
                 RETURN query SELECT * FROM isnt_empty(q1);
             ELSE
-                RETURN query SELECT * FROM is_empty(q1);
+                IF func='pgr_alphashape' THEN
+                    RETURN query SELECT * FROM isnt_empty(q1);
+                ELSE
+                    RETURN query SELECT * FROM is_empty(q1);
+                END IF;
             END IF;
         END IF;
-
     END LOOP;
 
 END
