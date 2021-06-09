95c79a4fc7399ea415c02852c69ebc71ece9bf10
cvvergara <vicky@georepublic.de>
2021-06-09 15:51:05 -0500

    cleaning up version

--- src/version/_version.c
+++ src/version/_version.c
@@ -25,7 +25,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  ********************************************************************PGR-GNU*/
 
 #include "c_common/postgres_connection.h"
-#include "./_version.h"
+#include "./version.h"
 
 #define UNUSED(x) (void)(x)
 
