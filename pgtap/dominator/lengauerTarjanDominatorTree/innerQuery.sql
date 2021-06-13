1f02488b4047fdc1f67571ffccf5d76d1099fdf1
cvvergara <vicky@georepublic.de>
2021-06-13 18:34:48 -0500

    not testing pgr_lengauerTarjanDominatorTree when version less than 3.2.0

--- pgtap/dominator/lengauerTarjanDominatorTree/innerQuery.sql
+++ pgtap/dominator/lengauerTarjanDominatorTree/innerQuery.sql
@@ -3,17 +3,25 @@
 UPDATE edge_table SET cost = sign(cost), reverse_cost = sign(reverse_cost);
 SELECT plan(54);
 
-/*
-SELECT has_function('pgr_lengauer_tarjan_dominator_tree',
-    ARRAY['text', 'bigint']);
 
-SELECT function_returns('pgr_lengauer_tarjan_dominator_tree',
-    ARRAY['text', 'bigint'],
-    'setof record');
-*/
+CREATE OR REPLACE FUNCTION inner_query()
+RETURNS SETOF TEXT AS
+$BODY$
+BEGIN
 
--- ONE TO ONE
+IF is_version_2() OR NOT test_min_version('3.2.0') THEN
+  RETURN QUERY
+  SELECT skip(54, 'Function is new on 3.2.0');
+  RETURN;
+END IF;
+
 SELECT style_dijkstra('pgr_lengauerTarjanDominatorTree', ',1)');
+
+END;
+$BODY$
+LANGUAGE plpgsql;
+
+SELECT inner_query();
 
 SELECT finish();
 ROLLBACK;
--- pgtap/dominator/lengauerTarjanDominatorTree/no_crash_test.sql
+++ pgtap/dominator/lengauerTarjanDominatorTree/no_crash_test.sql
@@ -6,31 +6,39 @@ SELECT plan(7);
 PREPARE edges AS
 SELECT id, source, target, cost, reverse_cost  FROM edge_table;
 
-SELECT isnt_empty('edges', 'Should be not empty to tests be meaningful');
 
-CREATE OR REPLACE FUNCTION test_function()
+CREATE OR REPLACE FUNCTION no_crash()
 RETURNS SETOF TEXT AS
 $BODY$
 DECLARE
 params TEXT[];
 subs TEXT[];
 BEGIN
-params = ARRAY[
-        '$$SELECT id, source, target, cost, reverse_cost  FROM edge_table$$',
-        '1::BIGINT'
-            ]::TEXT[];
-    subs = ARRAY[
-    'NULL',
-    '(SELECT id FROM edge_table_vertices_pgr  WHERE id IN (-1))'
-    ]::TEXT[];
+IF is_version_2() OR NOT test_min_version('3.2.0') THEN
+  RETURN QUERY
+  SELECT skip(7, 'Function is new on 3.2.0');
+  RETURN;
+END IF;
 
-    RETURN query SELECT * FROM no_crash_test('pgr_lengauerTarjanDominatorTree', params, subs);
+RETURN QUERY
+SELECT isnt_empty('edges', 'Should be not empty to tests be meaningful');
+
+params = ARRAY[
+  '$$SELECT id, source, target, cost, reverse_cost  FROM edge_table$$',
+  '1::BIGINT'
+  ]::TEXT[];
+subs = ARRAY[
+  'NULL',
+  '(SELECT id FROM edge_table_vertices_pgr  WHERE id IN (-1))'
+  ]::TEXT[];
+
+RETURN query SELECT * FROM no_crash_test('pgr_lengauerTarjanDominatorTree', params, subs);
 END
 $BODY$
 LANGUAGE plpgsql VOLATILE;
 
 
-SELECT * FROM test_function();
+SELECT * FROM no_crash();
 
 
 
