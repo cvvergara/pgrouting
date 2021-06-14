93d98e8abc24ea85a70e6998a389b9a1f8c4dcc6
cvvergara <vicky@georepublic.de>
2021-06-14 13:38:30 -0500

    not testing pgr_bellmanford when version less than 3.2.0

--- pgtap/bellman_ford/bellman_ford/bellman_ford-typesCheck.sql
+++ pgtap/bellman_ford/bellman_ford/bellman_ford-typesCheck.sql
@@ -8,12 +8,10 @@ SELECT has_function('pgr_bellmanford', ARRAY['text','bigint','bigint','boolean']
 SELECT has_function('pgr_bellmanford', ARRAY['text','bigint','anyarray','boolean']);
 SELECT has_function('pgr_bellmanford', ARRAY['text','anyarray','bigint','boolean']);
 SELECT has_function('pgr_bellmanford', ARRAY['text','anyarray','anyarray','boolean']);
-SELECT has_function('pgr_bellmanford', ARRAY['text','text','boolean']);
 SELECT function_returns('pgr_bellmanford', ARRAY['text','bigint','bigint','boolean'], 'setof record');
 SELECT function_returns('pgr_bellmanford', ARRAY['text','bigint','anyarray','boolean'], 'setof record');
 SELECT function_returns('pgr_bellmanford', ARRAY['text','anyarray','bigint','boolean'], 'setof record');
 SELECT function_returns('pgr_bellmanford', ARRAY['text','anyarray','anyarray','boolean'], 'setof record');
-SELECT function_returns('pgr_bellmanford', ARRAY['text','text','boolean'], 'setof record');
 
 -- testing column names
 SELECT bag_has(
@@ -36,10 +34,25 @@ SELECT bag_has(
     $$SELECT  '{"","","","directed","seq","path_seq","start_vid","end_vid","node","edge","cost","agg_cost"}'::TEXT[] $$
 );
 
-SELECT bag_has(
-    $$SELECT  proargnames from pg_proc where proname = 'pgr_bellmanford'$$,
-    $$SELECT  '{"","",directed,seq,path_seq,start_vid,end_vid,node,edge,cost,agg_cost}'::TEXT[] $$
-);
+
+-- new signature on 3.2
+SELECT CASE
+WHEN is_version_2() OR NOT test_min_version('3.2.0') THEN
+  skip(3, 'Combinations functiontionality new on 2.3')
+WHEN test_min_version('3.2.0') THEN
+  collect_tap(
+    has_function('pgr_bellmanford', ARRAY['text','text','boolean']),
+    function_returns('pgr_bellmanford', ARRAY['text','text','boolean'], 'setof record'),
+    bag_has(
+      $$SELECT  proargnames from pg_proc where proname = 'pgr_bellmanford'$$,
+      $$SELECT  '{"","",directed,seq,path_seq,start_vid,end_vid,node,edge,cost,agg_cost}'::TEXT[] $$
+    ),
+ set_eq(
+    $$SELECT  proallargtypes from pg_proc where proname = 'pgr_bellmanford'$$,
+    $$VALUES ('{25,25,16,23,23,20,20,20,20,701,701}'::OID[]) $$
+  )
+)
+END;
 
 -- parameter types
 SELECT set_eq(
@@ -48,7 +61,9 @@ SELECT set_eq(
         ('{25,20,20,16,23,23,20,20,701,701}'::OID[]),
         ('{25,20,2277,16,23,23,20,20,20,701,701}'::OID[]),
         ('{25,2277,20,16,23,23,20,20,20,701,701}'::OID[]),
-        ('{25,2277,2277,16,23,23,20,20,20,20,701,701}'::OID[]),
-        ('{25,25,16,23,23,20,20,20,20,701,701}'::OID[])
+        ('{25,2277,2277,16,23,23,20,20,20,20,701,701}'::OID[])
     $$
 );
+
+SELECT finish();
+ROLLBACK;
