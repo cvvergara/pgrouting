8151e4a8c730fff7f15a19a4c5e95d114717a6b9
cvvergara <vicky@georepublic.de>
2021-06-14 13:45:15 -0500

    not testing pgr_edwardMoore when version less than 3.2.0

--- pgtap/bellman_ford/edwardMoore/edwardMoore-types-check.sql
+++ pgtap/bellman_ford/edwardMoore/edwardMoore-types-check.sql
@@ -12,12 +12,10 @@ SELECT has_function('pgr_edwardmoore', ARRAY['text','bigint','bigint','boolean']
 SELECT has_function('pgr_edwardmoore', ARRAY['text','bigint','anyarray','boolean']);
 SELECT has_function('pgr_edwardmoore', ARRAY['text','anyarray','bigint','boolean']);
 SELECT has_function('pgr_edwardmoore', ARRAY['text','anyarray','anyarray','boolean']);
-SELECT has_function('pgr_edwardmoore', ARRAY['text','text','boolean']);
 SELECT function_returns('pgr_edwardmoore', ARRAY['text','bigint','bigint','boolean'],  'setof record');
 SELECT function_returns('pgr_edwardmoore', ARRAY['text','bigint','anyarray','boolean'],  'setof record');
 SELECT function_returns('pgr_edwardmoore', ARRAY['text','anyarray','bigint','boolean'],  'setof record');
 SELECT function_returns('pgr_edwardmoore', ARRAY['text','anyarray','anyarray','boolean'],  'setof record');
-SELECT function_returns('pgr_edwardmoore', ARRAY['text','text','boolean'],  'setof record');
 
 -- pgr_edwardmoore
 -- parameter names
@@ -41,10 +39,6 @@ SELECT bag_has(
     $$SELECT  '{"","","","directed","seq","path_seq","start_vid","end_vid","node","edge","cost","agg_cost"}'::TEXT[] $$
 );
 
-SELECT bag_has(
-    $$SELECT  proargnames from pg_proc where proname = 'pgr_edwardmoore'$$,
-    $$SELECT  '{"","",directed,seq,path_seq,start_vid,end_vid,node,edge,cost,agg_cost}'::TEXT[] $$
-);
 
 -- parameter types
 SELECT set_eq(
@@ -53,11 +47,28 @@ SELECT set_eq(
         ('{25,20,20,16,23,23,20,20,701,701}'::OID[]),
         ('{25,20,2277,16,23,23,20,20,20,701,701}'::OID[]),
         ('{25,2277,20,16,23,23,20,20,20,701,701}'::OID[]),
-        ('{25,2277,2277,16,23,23,20,20,20,20,701,701}'::OID[]),
-        ('{25,25,16,23,23,20,20,20,20,701,701}'::OID[])
+        ('{25,2277,2277,16,23,23,20,20,20,20,701,701}'::OID[])
     $$
 );
 
+-- new signature on 3.2
+SELECT CASE
+WHEN is_version_2() OR NOT test_min_version('3.2.0') THEN
+  skip(3, 'Combinations functiontionality new on 2.3')
+WHEN test_min_version('3.2.0') THEN
+  collect_tap(
+    has_function('pgr_edwardmoore', ARRAY['text','text','boolean']),
+    function_returns('pgr_edwardmoore', ARRAY['text','text','boolean'], 'setof record'),
+    bag_has(
+      $$SELECT  proargnames from pg_proc where proname = 'pgr_edwardmoore'$$,
+      $$SELECT  '{"","",directed,seq,path_seq,start_vid,end_vid,node,edge,cost,agg_cost}'::TEXT[] $$
+    ),
+ set_eq(
+    $$SELECT  proallargtypes from pg_proc where proname = 'pgr_edwardmoore'$$,
+    $$VALUES ('{25,25,16,23,23,20,20,20,20,701,701}'::OID[]) $$
+  )
+)
+END;
 
 SELECT * FROM finish();
 ROLLBACK;
--- pgtap/bellman_ford/edwardMoore/empty_combinations_empty_result.test.sql
+++ pgtap/bellman_ford/edwardMoore/empty_combinations_empty_result.test.sql
@@ -7,6 +7,12 @@ RETURNS SETOF TEXT AS
 $BODY$
 BEGIN
 
+  IF is_version_2() OR NOT test_min_version('3.2.0') THEN
+    RETURN QUERY
+    SELECT skip(1, 'Combinations signature is new on 3.2.0');
+    RETURN;
+  END IF;
+
     RETURN query SELECT is_empty(
       'SELECT seq, path_seq, node, edge, cost, agg_cost FROM pgr_edwardMoore(
         ''SELECT id, source, target, cost, reverse_cost FROM edge_table'',
