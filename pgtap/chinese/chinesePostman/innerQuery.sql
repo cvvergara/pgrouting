dca0e1c098670bd24c7cb24eb19269269c0fda99
cvvergara <vicky@georepublic.de>
2021-06-21 14:52:39 -0500

    more on adjusting tests

--- pgtap/chinese/chinesePostman/innerQuery.sql
+++ pgtap/chinese/chinesePostman/innerQuery.sql
@@ -1,7 +1,7 @@
 \i setup.sql
 
 UPDATE edge_table SET cost = sign(cost), reverse_cost = sign(reverse_cost);
-SELECT plan(56);
+SELECT CASE WHEN is_version_2() THEN plan(1) ELSE plan(54) END;
 
 CREATE OR REPLACE FUNCTION inner_query()
 RETURNS SETOF TEXT AS
@@ -10,7 +10,7 @@ BEGIN
 
 IF is_version_2() THEN
   RETURN QUERY
-  SELECT skip(56, 'Function is new on 3.0.0');
+  SELECT skip(1, 'Function is new on 3.0.0');
   RETURN;
 END IF;
 
@@ -25,6 +25,12 @@ SELECT function_returns('pgr_chinesepostman',
     'setof record');
 
 DELETE FROM edge_table WHERE id > 16;
+
+IF min_lib_version('3.1.1') THEN
+  RETURN QUERY
+  SELECT skip(52, 'Function is new on 3.0.0');
+  RETURN;
+END IF;
 
 RETURN QUERY
 SELECT style_dijkstra('pgr_chinesepostman', ')');
