ef8527ecc80eda8562805586399edb0a5a3a14b4
cvvergara <vicky@georepublic.de>
2021-06-28 20:56:07 -0500

    refining pgtap

--- pgtap/tsp/tsp/edge_cases.sql
+++ pgtap/tsp/tsp/edge_cases.sql
@@ -1,5 +1,8 @@
 \i setup.sql
+
 UPDATE edge_table SET cost = sign(cost) + 0.001 * id * id, reverse_cost = sign(reverse_cost) + 0.001 * id * id;
+SELECT CASE WHEN min_lib_version('3.2.1') THEN plan(20) ELSE plan(1) END;
+
 
 CREATE TEMP TABLE data AS
 SELECT * FROM pgr_withPointsCostMatrix(
@@ -7,73 +10,74 @@ SELECT * FROM pgr_withPointsCostMatrix(
   'SELECT pid, edge_id, fraction from pointsOfInterest',
   array[-1, 3, 5, 6, -6], directed := false);
 
-SELECT CASE WHEN min_lib_version('3.2.1') THEN plan(20) ELSE plan(1) END;
 
 CREATE FUNCTION tsp_edge_cases()
 RETURNS SETOF TEXT AS
 $code$
 BEGIN
-  IF NOT min_lib_version('3.2.1') THEN RETURN QUERY SELECT skip(1, 'Throws about start_id and end_id in 3.2.1'); END IF;
+  IF NOT min_lib_version('3.2.1') THEN
+    RETURN QUERY SELECT skip(1, 'Throws about start_id and end_id in 3.2.1');
 
-  RETURN QUERY
-  SELECT throws_ok($$
-    SELECT * FROM pgr_TSP('SELECT * FROM data', start_id => 5, end_id => 10) $$,
-    'XX000',
-    'start_id or end_id do not exist on the data',
-    '1 SHOULD throw because end_id does not exist');
+  ELSE
 
-  RETURN QUERY
-  SELECT throws_ok($$
-    SELECT * FROM pgr_TSP('SELECT * FROM data', end_id => 10) $$,
-    'XX000',
-    'start_id or end_id do not exist on the data',
-    '2 SHOULD throw because end_id does not exist');
+    RETURN QUERY
+    SELECT throws_ok($$
+      SELECT * FROM pgr_TSP('SELECT * FROM data', start_id => 5, end_id => 10) $$,
+      'XX000',
+      'start_id or end_id do not exist on the data',
+      '1 SHOULD throw because end_id does not exist');
 
-  RETURN QUERY
-  SELECT throws_ok($$
-    SELECT * FROM pgr_TSP('SELECT * FROM data', start_id => 10, end_id => 5) $$,
-    'XX000',
-    'start_id or end_id do not exist on the data',
-    '3 SHOULD throw because start_id does not exist');
+    RETURN QUERY
+    SELECT throws_ok($$
+      SELECT * FROM pgr_TSP('SELECT * FROM data', end_id => 10) $$,
+      'XX000',
+      'start_id or end_id do not exist on the data',
+      '2 SHOULD throw because end_id does not exist');
 
-  RETURN QUERY
-  SELECT throws_ok($$
-    SELECT * FROM pgr_TSP('SELECT * FROM data', start_id => 10) $$,
-    'XX000',
-    'start_id or end_id do not exist on the data',
-    '4 SHOULD throw because start_id does not exist');
+    RETURN QUERY
+    SELECT throws_ok($$
+      SELECT * FROM pgr_TSP('SELECT * FROM data', start_id => 10, end_id => 5) $$,
+      'XX000',
+      'start_id or end_id do not exist on the data',
+      '3 SHOULD throw because start_id does not exist');
 
-  RETURN QUERY
-  SELECT is(
-    (SELECT cost FROM pgr_TSP('SELECT * FROM data', start_id => 5, end_id => 3) WHERE seq = 1),
-    0::FLOAT,
-    '5 SHOULD PASS: cost at row 0 is 0.0');
+    RETURN QUERY
+    SELECT throws_ok($$
+      SELECT * FROM pgr_TSP('SELECT * FROM data', start_id => 10) $$,
+      'XX000',
+      'start_id or end_id do not exist on the data',
+      '4 SHOULD throw because start_id does not exist');
 
-  RETURN QUERY
-  SELECT is(
-    (SELECT node FROM pgr_TSP('SELECT * FROM data', end_id => 3) WHERE seq = 1),
-    3::BIGINT,
-    '6: end_id => 3 SHOULD PASS: first node should be 3');
+    RETURN QUERY
+    SELECT is(
+      (SELECT cost FROM pgr_TSP('SELECT * FROM data', start_id => 5, end_id => 3) WHERE seq = 1),
+      0::FLOAT,
+      '5 SHOULD PASS: cost at row 0 is 0.0');
 
-  RETURN QUERY
-  SELECT is(
-    (SELECT cost FROM pgr_TSP('SELECT * FROM data', end_id => 3) WHERE seq = 1),
-    0::FLOAT,
-    '7: end_id => 3 SHOULD PASS: cost at row 0 is 0.0');
+    RETURN QUERY
+    SELECT is(
+      (SELECT node FROM pgr_TSP('SELECT * FROM data', end_id => 3) WHERE seq = 1),
+      3::BIGINT,
+      '6: end_id => 3 SHOULD PASS: first node should be 3');
 
-  RETURN QUERY
-  SELECT is(
-    (SELECT cost FROM pgr_TSP('SELECT * FROM data', start_id => 5) WHERE seq = 1),
-    0::FLOAT,
-    '8: start_id => 5 SHOULD PASS: cost at row 0 is 0.0');
+    RETURN QUERY
+    SELECT is(
+      (SELECT cost FROM pgr_TSP('SELECT * FROM data', end_id => 3) WHERE seq = 1),
+      0::FLOAT,
+      '7: end_id => 3 SHOULD PASS: cost at row 0 is 0.0');
 
-  RETURN QUERY
-  SELECT is(
-    (SELECT node FROM pgr_TSP('SELECT * FROM data', end_id => 3) WHERE seq = 6),
-    3::BIGINT,
-    '9: end_id => 3 SHOULD PASS: last node should be 3');
+    RETURN QUERY
+    SELECT is(
+      (SELECT cost FROM pgr_TSP('SELECT * FROM data', start_id => 5) WHERE seq = 1),
+      0::FLOAT,
+      '8: start_id => 5 SHOULD PASS: cost at row 0 is 0.0');
 
-  IF NOT min_lib_version('3.3.0') THEN PERFORM todo_end(); END IF;
+    RETURN QUERY
+    SELECT is(
+      (SELECT node FROM pgr_TSP('SELECT * FROM data', end_id => 3) WHERE seq = 6),
+      3::BIGINT,
+      '9: end_id => 3 SHOULD PASS: last node should be 3');
+  END IF;
 
   RETURN QUERY
   SELECT is(
