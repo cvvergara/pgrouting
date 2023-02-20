a2bb164eb6afc735f8713bbd77615290a4b6bad3
cvvergara <vicky@georepublic.de>
2023-02-20 10:11:28 -0600

    [allpairs] code works for floydWarshall

--- src/allpairs/floydWarshall.c
+++ src/allpairs/floydWarshall.c
@@ -35,13 +35,17 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include "c_common/e_report.h"
 #include "c_common/time_msg.h"
 #include "c_common/edges_input.h"
+#include "allpairs/process_allpairs.h"
+
+#if 0
 
 #include "drivers/allpairs/floydWarshall_driver.h"
+#endif
 
 PGDLLEXPORT Datum _pgr_floydwarshall(PG_FUNCTION_ARGS);
 PG_FUNCTION_INFO_V1(_pgr_floydwarshall);
 
-#if 1
+#if 0
 static
 void process(
         char* edges_sql,
@@ -114,9 +118,10 @@ _pgr_floydwarshall(PG_FUNCTION_ARGS) {
 
 
         PGR_DBG("Calling process");
-        process(
+        process_allpairs(
                 text_to_cstring(PG_GETARG_TEXT_P(0)),
                 PG_GETARG_BOOL(1),
+                1, /* Floyd Warshall */
                 &result_tuples,
                 &result_count);
 
