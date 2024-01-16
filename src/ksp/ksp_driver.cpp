4387c8b8ceadcec875d5e817d9f4044c69a1bc9a
cvvergara <vicky@erosion.dev>
2024-01-16 11:20:07 -0600

    [ksp_driver.cpp] Removing some compilation errors

--- src/ksp/ksp_driver.cpp
+++ src/ksp/ksp_driver.cpp
@@ -46,8 +46,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include "c_types/ii_t_rt.h"
 
 void  pgr_do_ksp(
-        Edge_t *data_edges, size_t total_edges,
-        II_t_rt *combinationsArr, size_t total_combinations,
+        char *edges_sql,
+        char *combinations_sql,
         int64_t*  start_vids, size_t size_start_vids,
         int64_t * end_vids, size_t size_end_vids,
         size_t k,
@@ -67,29 +67,44 @@ void  pgr_do_ksp(
     std::ostringstream err;
     std::ostringstream log;
     std::ostringstream notice;
+    char *hint = nullptr;
     try {
         pgassert(!(*log_msg));
         pgassert(!(*notice_msg));
         pgassert(!(*err_msg));
         pgassert(!(*return_tuples));
         pgassert(*return_count == 0);
-        pgassert(total_edges != 0);
-
-        auto combinations = total_combinations?
-            pgrouting::utilities::get_combinations(combinationsArr, total_combinations)
-            : pgrouting::utilities::get_combinations(start_vids, size_start_vids, end_vids, size_end_vids);
 
         graphType gType = directed? DIRECTED: UNDIRECTED;
+
+        hint = edges_sql;
+        auto edges = pgrouting::pgget::get_edges(std::string(edges_sql), normal, false);
+
+        if (edges.empty()) {
+            *notice_msg = pgr_msg("No edges found");
+            *log_msg = hint? pgr_msg(hint) : pgr_msg(log.str().c_str());
+            return;
+        }
+        hint = nullptr;
+
+        hint = combinations_sql;
+        auto combinationsArr = combinations_sql?
+            pgrouting::pgget::get_combinations(std::string(combinations_sql)) : std::vector<II_t_rt>();
+        hint = nullptr;
+
+        auto combinations = combinationsArr.empty()?
+            pgrouting::utilities::get_combinations(start_vidsArr, size_start_vidsArr, end_vidsArr, size_end_vidsArr)
+            : pgrouting::utilities::get_combinations(combinationsArr);
 
         std::deque< Path > paths;
 
         if (directed) {
             pgrouting::DirectedGraph digraph(gType);
-            digraph.insert_edges(data_edges, total_edges);
+            digraph.insert_edges(edges);
             paths = pgrouting::algorithms::Yen(digraph, combinations, k, heap_paths);
         } else {
             pgrouting::UndirectedGraph undigraph(gType);
-            undigraph.insert_edges(data_edges, total_edges);
+            undigraph.insert_edges(edges);
             paths = pgrouting::algorithms::Yen(undigraph, combinations, k, heap_paths);
         }
 
