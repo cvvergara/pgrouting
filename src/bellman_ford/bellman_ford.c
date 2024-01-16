8df619a09e7990a9b41cdc6879ecf30ae16f661d
cvvergara <vicky@erosion.dev>
2024-01-15 19:08:49 -0600

    [bellman_ford.c] Using new driver and marking code to be deleted

--- src/bellman_ford/bellman_ford.c
+++ src/bellman_ford/bellman_ford.c
@@ -60,7 +60,9 @@ process(
     char* notice_msg = NULL;
     char* err_msg = NULL;
 
+#if 0
     PGR_DBG("Initializing arrays");
+#endif
 
     size_t size_start_vidsArr = 0;
     int64_t* start_vidsArr = NULL;
@@ -68,8 +70,10 @@ process(
     size_t size_end_vidsArr = 0;
     int64_t* end_vidsArr = NULL;
 
+#if 0
     size_t total_combinations = 0;
     II_t_rt *combinations = NULL;
+#endif
 
     if (starts && ends) {
         start_vidsArr = pgr_get_bigIntArray(&size_start_vidsArr, starts, false, &err_msg);
@@ -77,6 +81,7 @@ process(
         end_vidsArr = pgr_get_bigIntArray(&size_end_vidsArr, ends, false, &err_msg);
         throw_error(err_msg, "While getting end vids");
     } else if (combinations_sql) {
+#if 0
         pgr_get_combinations(combinations_sql, &combinations, &total_combinations, &err_msg);
         throw_error(err_msg, combinations_sql);
         if (total_combinations == 0) {
@@ -85,8 +90,10 @@ process(
             pgr_SPI_finish();
             return;
         }
+#endif
     }
 
+#if 0
     (*result_tuples) = NULL;
     (*result_count) = 0;
 
@@ -106,12 +113,11 @@ process(
     }
 
     PGR_DBG("Starting processing");
+#endif
     clock_t start_t = clock();
-    do_pgr_bellman_ford(
-            edges,
-            total_edges,
-            combinations,
-            total_combinations,
+    pgr_do_bellman_ford(
+            edges_sql,
+            combinations_sql,
             start_vidsArr, size_start_vidsArr,
             end_vidsArr, size_end_vidsArr,
             directed,
@@ -125,15 +131,21 @@ process(
             &err_msg);
 
     time_msg(" processing pgr_bellman_ford", start_t, clock());
+#if 0
     PGR_DBG("Returning %ld tuples", *result_count);
+#endif
 
-    if (err_msg) {
-        if (*result_tuples) pfree(*result_tuples);
+    if (err_msg && (*result_tuples)) {
+        pfree(*result_tuples);
+        (*result_tuples) = NULL;
+        (*result_count) = 0;
     }
 
     pgr_global_report(log_msg, notice_msg, err_msg);
 
+#if 0
     if (edges) pfree(edges);
+#endif
     if (log_msg) pfree(log_msg);
     if (notice_msg) pfree(notice_msg);
     if (err_msg) pfree(err_msg);
@@ -148,10 +160,8 @@ PGDLLEXPORT Datum _pgr_bellmanford(PG_FUNCTION_ARGS) {
     FuncCallContext     *funcctx;
     TupleDesc           tuple_desc;
 
-    /**************************************************************************/
     Path_rt  *result_tuples = NULL;
     size_t result_count = 0;
-    /**************************************************************************/
 
     if (SRF_IS_FIRSTCALL()) {
         MemoryContext   oldcontext;
@@ -159,7 +169,9 @@ PGDLLEXPORT Datum _pgr_bellmanford(PG_FUNCTION_ARGS) {
         oldcontext = MemoryContextSwitchTo(funcctx->multi_call_memory_ctx);
 
 
+#if 0
         PGR_DBG("Calling process");
+#endif
         if (PG_NARGS() == 5) {
             /*
              * many to many
@@ -189,9 +201,6 @@ PGDLLEXPORT Datum _pgr_bellmanford(PG_FUNCTION_ARGS) {
                 &result_count);
         }
 
-
-        /**********************************************************************/
-
         funcctx->max_calls = result_count;
         funcctx->user_fctx = result_tuples;
         if (get_call_result_type(fcinfo, NULL, &tuple_desc)
@@ -216,18 +225,6 @@ PGDLLEXPORT Datum _pgr_bellmanford(PG_FUNCTION_ARGS) {
         Datum        *values;
         bool*        nulls;
 
-        /**********************************************************************/
-        /*
-            OUT seq INTEGER,
-            OUT path_seq INTEGER,
-            OUT start_vid BIGINT,
-            OUT end_vid BIGINT,
-            OUT node BIGINT,
-            OUT edge BIGINT,
-            OUT cost FLOAT,
-            OUT agg_cost FLOAT
-        */
-        /**********************************************************************/
         size_t numb = 8;
         values = palloc(numb * sizeof(Datum));
         nulls = palloc(numb * sizeof(bool));
@@ -247,18 +244,10 @@ PGDLLEXPORT Datum _pgr_bellmanford(PG_FUNCTION_ARGS) {
         values[6] = Float8GetDatum(result_tuples[funcctx->call_cntr].cost);
         values[7] = Float8GetDatum(result_tuples[funcctx->call_cntr].agg_cost);
 
-        /**********************************************************************/
-
         tuple = heap_form_tuple(tuple_desc, values, nulls);
         result = HeapTupleGetDatum(tuple);
         SRF_RETURN_NEXT(funcctx, result);
     } else {
-        /**********************************************************************/
-
-        PGR_DBG("Clean up code");
-
-        /**********************************************************************/
-
         SRF_RETURN_DONE(funcctx);
     }
 }
