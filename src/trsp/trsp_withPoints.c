09b8eceeb362ed798be942c8ea4fd1d2cef763aa
cvvergara <vicky@erosion.dev>
2024-01-16 10:16:33 -0600

    [trsp_withPoints.c] Marking include to be deleted

--- src/trsp/trsp_withPoints.c
+++ src/trsp/trsp_withPoints.c
@@ -39,7 +39,9 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include "c_common/e_report.h"
 #include "c_common/time_msg.h"
 
+#if 0
 #include "c_common/pgdata_getters.h"
+#endif
 
 #include "drivers/withPoints/get_new_queries.h"
 #include "drivers/trsp/trsp_withPoints_driver.h"
