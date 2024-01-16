ef2c4544258386e4f19b4e605cefdde2d4f473fb
cvvergara <vicky@erosion.dev>
2024-01-16 10:30:33 -0600

    [trspVia.c] Files with ArrayType need pgdata_getters

--- src/trsp/trspVia.c
+++ src/trsp/trspVia.c
@@ -31,9 +31,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include "c_common/debug_macro.h"
 #include "c_common/e_report.h"
 #include "c_common/time_msg.h"
-#if 0
 #include "c_common/pgdata_getters.h"
-#endif
 #include "drivers/trsp/trspVia_driver.h"
 
 PGDLLEXPORT Datum _pgr_trspvia(PG_FUNCTION_ARGS);
