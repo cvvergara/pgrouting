1a647f9fbc316cdcdb914dbea253cad423fa14ea
cvvergara <vicky@erosion.dev>
2024-01-16 10:16:33 -0600

    [bdDijkstra.c] Marking include to be deleted

--- src/bdDijkstra/bdDijkstra.c
+++ src/bdDijkstra/bdDijkstra.c
@@ -37,7 +37,9 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include "c_common/e_report.h"
 #include "c_common/time_msg.h"
 
+#if 0
 #include "c_common/pgdata_getters.h"
+#endif
 
 #include "drivers/bdDijkstra/bdDijkstra_driver.h"
 
