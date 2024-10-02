725c4a8c305484ad8ca4d3dbfe988fc4259a7560
cvvergara <vicky@erosion.dev>
2024-10-01 22:33:08 -0600

    linting

--- src/withPoints/withPoints_driver.cpp
+++ src/withPoints/withPoints_driver.cpp
@@ -38,8 +38,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include <cassert>
 #include <limits>
 #include <string>
-#include <map>
-#include <set>
 
 #include "withPoints/withPoints.hpp"
 #include "cpp_common/pgdata_getters.hpp"
