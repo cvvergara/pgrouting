c024dae1769df8ac8a0c88ca5ad9776cedee3822
Aurélie Bousquet <aurelie.bousquet@oslandia.com>
2025-01-14 17:39:54 +0100

    Include libraries

--- include/contraction/contractionGraph.hpp
+++ include/contraction/contractionGraph.hpp
@@ -36,7 +36,9 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #include <vector>
 #include <iostream>
 #include <tuple>
-#include <cstdint>
+#include <functional>
+#include <utility>
+#include <queue>
 
 #include <boost/graph/iteration_macros.hpp>
 
