3da81738b8579e5dd77fd4ddebe92aa343a7afd4
Aurélie Bousquet <aurelie.bousquet@oslandia.com>
2025-01-16 10:27:06 +0100

    Class contractionGraph

--- include/contraction/linearContraction.hpp
+++ include/contraction/linearContraction.hpp
@@ -81,7 +81,7 @@ class Pgr_linear {
 
 
      void doContraction(G &graph, Identifiers<V> forbidden_vertices) {
-         m_forbiddenVertices = forbidden_vertices;
+         graph.setForbiddenVertices(forbidden_vertices);
          calculateVertices(graph);
 
          while (!m_linearVertices.empty()) {
