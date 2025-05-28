68a0fabd0e85b742cba8b4fb3d103a90ebdb4591
cvvergara <vicky@erosion.dev>
2025-05-28 17:56:49 -0600

    Some update tests pass

--- sql/scripts/build-extension-update-files.pl
+++ sql/scripts/build-extension-update-files.pl
@@ -261,6 +261,24 @@ sub generate_upgrade_script {
             push @commands, drop_special_case_function("pgr_kruskaldd(text,anyarray,numeric)");
             push @commands, drop_special_case_function("pgr_kruskaldd(text,anyarray,double precision)");
         }
+        if ($old_mayor <= 4.0) {
+            # Out parameters changed names on v4.0.0
+            push @commands, drop_special_case_function("pgr_withpoints(text,text,anyarray,anyarray,boolean,character,boolean)");
+            push @commands, drop_special_case_function("pgr_withpoints(text,text,anyarray,bigint,boolean,character,boolean)");
+            push @commands, drop_special_case_function("pgr_withpoints(text,text,bigint,anyarray,boolean,character,boolean)");
+            push @commands, drop_special_case_function("pgr_withpoints(text,text,bigint,bigint,boolean,character,boolean)");
+            push @commands, drop_special_case_function("pgr_withpoints(text,text,text,boolean,character,boolean)");
+
+            # Out parameters changed names on v4.0.0
+            push @commands, drop_special_case_function("pgr_withpointscost(text,text,anyarray,anyarray,boolean,character)");
+            push @commands, drop_special_case_function("pgr_withpointscost(text,text,anyarray,bigint,boolean,character)");
+            push @commands, drop_special_case_function("pgr_withpointscost(text,text,bigint,anyarray,boolean,character)");
+            push @commands, drop_special_case_function("pgr_withpointscost(text,text,bigint,bigint,boolean,character)");
+            push @commands, drop_special_case_function("pgr_withpointscost(text,text,text,boolean,character)");
+
+            # Out parameters changed names on v4.0.0
+            push @commands, drop_special_case_function("pgr_withpointscostmatrix(text,text,anyarray,boolean,character)");
+        }
     }
 
     #------------------------------------
