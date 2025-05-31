68662d37383c579dd2301966242ad54cbbb29a8c
cvvergara <vicky@erosion.dev>
2025-05-31 10:15:50 -0600

    [sql/bellman_ford/bellman_ford.sql] Standardizing output columns

--- sql/bellman_ford/bellman_ford.sql
+++ sql/bellman_ford/bellman_ford.sql
@@ -4,9 +4,8 @@ File: bellman_ford.sql
 Copyright (c) 2015 pgRouting developers
 Mail: project@pgrouting.org
 
-Function's developer:
 Copyright (c) 2018 Sourabh Garg
-Mail: sourabh.garg.mat@gmail.com
+Mail: sourabh.garg.mat at gmail.com
 
 ------
 
@@ -26,12 +25,12 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
  ********************************************************************PGR-GNU*/
 
---ONE TO ONE
+-- ONE TO ONE
 --v3.0
 CREATE FUNCTION pgr_bellmanFord(
-    TEXT,   -- edges_sql (required)
-    BIGINT, -- from_vid (required)
-    BIGINT, -- to_vid (required)
+    TEXT,   -- edges sql (required)
+    BIGINT, -- from vid (required)
+    BIGINT, -- to vid (required)
 
     directed BOOLEAN DEFAULT true,
 
@@ -43,7 +42,6 @@ CREATE FUNCTION pgr_bellmanFord(
     OUT edge BIGINT,
     OUT cost FLOAT,
     OUT agg_cost FLOAT)
-
 RETURNS SETOF RECORD AS
 $BODY$
     SELECT seq, path_seq, start_vid, end_vid, node, edge, cost, agg_cost
@@ -52,15 +50,14 @@ $BODY$
 LANGUAGE SQL VOLATILE STRICT;
 
 
---ONE TO MANY
+-- ONE TO MANY
 --v3.0
 CREATE FUNCTION pgr_bellmanFord(
-    TEXT,     -- edges_sql (required)
-    BIGINT,   -- from_vid (required)
-    ANYARRAY, -- to_vids (required)
+    TEXT,     -- edges sql (required)
+    BIGINT,   -- from vid (required)
+    ANYARRAY, -- to vids (required)
 
     directed BOOLEAN DEFAULT true,
-
 
     OUT seq INTEGER,
     OUT path_seq INTEGER,
@@ -70,7 +67,6 @@ CREATE FUNCTION pgr_bellmanFord(
     OUT edge BIGINT,
     OUT cost FLOAT,
     OUT agg_cost FLOAT)
-
 RETURNS SETOF RECORD AS
 $BODY$
     SELECT seq, path_seq, start_vid, end_vid, node, edge, cost, agg_cost
@@ -79,12 +75,12 @@ $BODY$
 LANGUAGE SQL VOLATILE STRICT;
 
 
---MANY TO ONE
+-- MANY TO ONE
 --v3.0
 CREATE FUNCTION pgr_bellmanFord(
-    TEXT,     -- edges_sql (required)
-    ANYARRAY, -- from_vids (required)
-    BIGINT,   -- to_vid (required)
+    TEXT,     -- edges sql (required)
+    ANYARRAY, -- from vids (required)
+    BIGINT,   -- to vid (required)
 
     directed BOOLEAN DEFAULT true,
 
@@ -96,7 +92,6 @@ CREATE FUNCTION pgr_bellmanFord(
     OUT edge BIGINT,
     OUT cost FLOAT,
     OUT agg_cost FLOAT)
-
 RETURNS SETOF RECORD AS
 $BODY$
     SELECT seq, path_seq, start_vid, end_vid, node, edge, cost, agg_cost
@@ -105,12 +100,12 @@ $BODY$
 LANGUAGE SQL VOLATILE STRICT;
 
 
---MANY TO MANY
+-- MANY TO MANY
 --v3.0
 CREATE FUNCTION pgr_bellmanFord(
-    TEXT,     -- edges_sql (required)
-    ANYARRAY, -- from_vids (required)
-    ANYARRAY, -- to_vids (required)
+    TEXT,     -- edges sql (required)
+    ANYARRAY, -- from vids (required)
+    ANYARRAY, -- to vids (required)
 
     directed BOOLEAN DEFAULT true,
 
@@ -122,16 +117,15 @@ CREATE FUNCTION pgr_bellmanFord(
     OUT edge BIGINT,
     OUT cost FLOAT,
     OUT agg_cost FLOAT)
-
 RETURNS SETOF RECORD AS
 $BODY$
     SELECT seq, path_seq, start_vid, end_vid, node, edge, cost, agg_cost
     FROM _pgr_bellmanFord(_pgr_get_statement($1), $2::BIGINT[], $3::BIGINT[], directed, false );
 $BODY$
-LANGUAGE sql VOLATILE STRICT;
+LANGUAGE SQL VOLATILE STRICT;
 
 
---COMBINATIONS
+-- COMBINATIONS
 --v3.2
 CREATE FUNCTION pgr_bellmanFord(
     TEXT,     -- edges_sql (required)
@@ -147,7 +141,6 @@ CREATE FUNCTION pgr_bellmanFord(
     OUT edge BIGINT,
     OUT cost FLOAT,
     OUT agg_cost FLOAT)
-
 RETURNS SETOF RECORD AS
 $BODY$
     SELECT seq, path_seq, start_vid, end_vid, node, edge, cost, agg_cost
@@ -156,14 +149,11 @@ $BODY$
 LANGUAGE SQL VOLATILE STRICT;
 
 
--- COMMENTS
-
-
 COMMENT ON FUNCTION pgr_bellmanFord(TEXT, BIGINT, BIGINT, BOOLEAN)
 IS 'pgr_bellmanFord(One to One)
 - EXPERIMENTAL
 - Parameters:
-  - edges SQL with columns: id, source, target, cost [,reverse_cost]
+  - Edges SQL with columns: id, source, target, cost [,reverse_cost]
   - From vertex identifier
   - To vertex identifier
 - Optional Parameters:
@@ -171,7 +161,6 @@ IS 'pgr_bellmanFord(One to One)
 - Documentation:
   - ${PROJECT_DOC_LINK}/pgr_bellmanFord.html
 ';
-
 
 COMMENT ON FUNCTION pgr_bellmanFord(TEXT, BIGINT, ANYARRAY, BOOLEAN)
 IS 'pgr_bellmanFord(One to Many)
@@ -186,7 +175,6 @@ IS 'pgr_bellmanFord(One to Many)
   - ${PROJECT_DOC_LINK}/pgr_bellmanFord.html
 ';
 
-
 COMMENT ON FUNCTION pgr_bellmanFord(TEXT, ANYARRAY, BIGINT, BOOLEAN)
 IS 'pgr_bellmanFord(Many to One)
 - EXPERIMENTAL
@@ -200,7 +188,6 @@ IS 'pgr_bellmanFord(Many to One)
   - ${PROJECT_DOC_LINK}/pgr_bellmanFord.html
 ';
 
-
 COMMENT ON FUNCTION pgr_bellmanFord(TEXT, ANYARRAY, ANYARRAY, BOOLEAN)
 IS 'pgr_bellmanFord(Many to Many)
 - EXPERIMENTAL
@@ -213,7 +200,6 @@ IS 'pgr_bellmanFord(Many to Many)
 - Documentation:
   - ${PROJECT_DOC_LINK}/pgr_bellmanFord.html
 ';
-
 
 COMMENT ON FUNCTION pgr_bellmanFord(TEXT, TEXT, BOOLEAN)
 IS 'pgr_bellmanFord(Combinations)
