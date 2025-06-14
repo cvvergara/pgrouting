a34cc2441f91facd60e4e5413fb0aaf9143fdb87
Vicky Vergara <vicky@erosion.dev>
2025-06-14 09:16:53 -0600

    (sql, src) pgr_withPointsKSP: driving side is unnamed compulsory parameter

--- sql/ksp/_withPointsKSP.sql
+++ sql/ksp/_withPointsKSP.sql
@@ -1,8 +1,11 @@
 /*PGR-GNU*****************************************************************
 File: _withPointsKSP.sql
 
+Copyright (c) 2015 pgRouting developers
+Mail: project at pgrouting.org
+
 Copyright (c) 2015 Celia Virginia Vergara Castillo
-vicky_vergara@hotmail.com
+Mail: vicky at erosion.dev
 
 Copyright (c) 2023 Abhinav Jain
 Mail: this.abhinav at gmail.com
@@ -24,10 +27,6 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
  ********************************************************************PGR-GNU*/
-
---------------------
--- pgr_withPointsKSP:
---------------------
 
 --v4.0
 CREATE FUNCTION _pgr_withPointsKSP_v4(
@@ -53,7 +52,7 @@ CREATE FUNCTION _pgr_withPointsKSP_v4(
   OUT agg_cost FLOAT)
 RETURNS SETOF RECORD AS
 'MODULE_PATHNAME'
-LANGUAGE c STABLE STRICT;
+LANGUAGE C VOLATILE STRICT;
 
 --v4.0
 CREATE FUNCTION _pgr_withPointsKSP_v4(
@@ -78,10 +77,92 @@ CREATE FUNCTION _pgr_withPointsKSP_v4(
   OUT agg_cost FLOAT)
 RETURNS SETOF RECORD AS
 'MODULE_PATHNAME'
-LANGUAGE c STABLE STRICT;
+LANGUAGE C VOLATILE STRICT;
 
 COMMENT ON FUNCTION _pgr_withPointsKSP_v4(TEXT, TEXT, ANYARRAY, ANYARRAY, INTEGER, CHAR, BOOLEAN, BOOLEAN, BOOLEAN)
 IS 'pgRouting internal function';
 
 COMMENT ON FUNCTION _pgr_withPointsKSP_v4(TEXT, TEXT, TEXT, INTEGER, CHAR, BOOLEAN, BOOLEAN, BOOLEAN)
 IS 'pgRouting internal function';
+
+/*TODO remove on v5*/
+--v3.6
+CREATE FUNCTION _pgr_withPointsKSP(
+  TEXT, -- edges
+  TEXT, -- points
+  ANYARRAY, -- departures
+  ANYARRAY, -- destinations
+  INTEGER, -- k
+  CHAR, -- driving side
+
+  BOOLEAN, -- directed
+  BOOLEAN, -- heaps
+  BOOLEAN, -- details
+  BOOLEAN, -- new ksp
+
+  OUT seq INTEGER,
+  OUT path_id INTEGER,
+  OUT path_seq INTEGER,
+  OUT start_vid BIGINT,
+  OUT end_vid BIGINT,
+  OUT node BIGINT,
+  OUT edge BIGINT,
+  OUT cost FLOAT,
+  OUT agg_cost FLOAT)
+RETURNS SETOF RECORD AS
+'MODULE_PATHNAME'
+LANGUAGE C VOLATILE STRICT;
+
+--v3.6
+CREATE FUNCTION _pgr_withPointsKSP(
+  TEXT, -- edges
+  TEXT, -- points
+  TEXT, -- combinations
+  INTEGER, -- k
+  CHAR, -- driving side
+
+  BOOLEAN, -- directed
+  BOOLEAN, -- heaps
+  BOOLEAN, -- details
+
+  OUT seq INTEGER,
+  OUT path_id INTEGER,
+  OUT path_seq INTEGER,
+  OUT start_vid BIGINT,
+  OUT end_vid BIGINT,
+  OUT node BIGINT,
+  OUT edge BIGINT,
+  OUT cost FLOAT,
+  OUT agg_cost FLOAT)
+RETURNS SETOF RECORD AS
+'MODULE_PATHNAME'
+LANGUAGE C VOLATILE STRICT;
+
+COMMENT ON FUNCTION _pgr_withPointsKSP(TEXT, TEXT, ANYARRAY, ANYARRAY, INTEGER, CHAR, BOOLEAN, BOOLEAN, BOOLEAN, BOOLEAN)
+IS 'pgRouting internal function deprecated on v4.0.0';
+
+COMMENT ON FUNCTION _pgr_withPointsKSP(TEXT, TEXT, TEXT, INTEGER, CHAR, BOOLEAN, BOOLEAN, BOOLEAN)
+IS 'pgRouting internal function deprecated on v4.0.0';
+
+--v3.0
+CREATE FUNCTION _pgr_withPointsKSP(
+    edges_sql TEXT,
+    points_sql TEXT,
+    start_pid BIGINT,
+    end_pid BIGINT,
+    k INTEGER,
+
+    directed BOOLEAN,
+    heap_paths BOOLEAN,
+    driving_side CHAR,
+    details BOOLEAN,
+
+    OUT seq INTEGER, OUT path_id INTEGER, OUT path_seq INTEGER,
+    OUT node BIGINT, OUT edge BIGINT,
+    OUT cost FLOAT, OUT agg_cost FLOAT)
+RETURNS SETOF RECORD AS
+'MODULE_PATHNAME'
+LANGUAGE C VOLATILE STRICT;
+
+COMMENT ON FUNCTION _pgr_withPointsKSP(TEXT, TEXT, BIGINT, BIGINT, INTEGER, BOOLEAN, BOOLEAN, CHAR, BOOLEAN)
+IS 'pgRouting internal function deprecated on v3.6.0';
