6c648876258fe7327c318fb54e231e96dab9eed1
Vicky Vergara <vicky@erosion.dev>
2025-06-14 09:05:49 -0600

    (sql,src) pgr_withPointsDD: driving side is unnamed compulsory parameter

--- src/driving_distance/driving_distance_withPoints.c
+++ src/driving_distance/driving_distance_withPoints.c
@@ -52,19 +52,25 @@ process(
         char *driving_side,
         bool details,
         bool equicost,
+        bool is_new,
 
         MST_rt **result_tuples,
         size_t *result_count) {
     char d_side = estimate_drivingSide(driving_side[0]);
-    if (d_side == ' ') {
-        pgr_throw_error("Invalid value of 'driving side'", "Valid value are 'r', 'l', 'b'");
-        return;
-    } else if (directed && !(d_side == 'r' || d_side == 'l')) {
-        pgr_throw_error("Invalid value of 'driving side'", "Valid values are for directed graph are: 'r', 'l'");
-        return;
-    } else if (!directed && !(d_side == 'b')) {
-        pgr_throw_error("Invalid value of 'driving side'", "Valid values are for undirected graph is: 'b'");
-        return;
+    if (is_new) {
+        if (d_side == ' ') {
+            pgr_throw_error("Invalid value of 'driving side'", "Valid value are 'r', 'l', 'b'");
+            return;
+        } else if (directed && !(d_side == 'r' || d_side == 'l')) {
+            pgr_throw_error("Invalid value of 'driving side'", "Valid values are for directed graph are: 'r', 'l'");
+            return;
+        } else if (!directed && !(d_side == 'b')) {
+            pgr_throw_error("Invalid value of 'driving side'", "Valid values are for undirected graph is: 'b'");
+            return;
+        }
+    } else {
+        d_side = (char)tolower(driving_side[0]);
+        if (!((d_side == 'r') || (d_side == 'l'))) d_side = 'b';
     }
 
     pgr_SPI_connect();
@@ -134,6 +140,7 @@ _pgr_withpointsddv4(PG_FUNCTION_ARGS) {
                 PG_GETARG_BOOL(6),
                 PG_GETARG_BOOL(7),
 
+                true,
                 &result_tuples, &result_count);
 
         funcctx->max_calls = result_count;
@@ -175,6 +182,102 @@ _pgr_withpointsddv4(PG_FUNCTION_ARGS) {
         values[5] = Int64GetDatum(result_tuples[funcctx->call_cntr].edge);
         values[6] = Float8GetDatum(result_tuples[funcctx->call_cntr].cost);
         values[7] = Float8GetDatum(result_tuples[funcctx->call_cntr].agg_cost);
+
+        tuple = heap_form_tuple(tuple_desc, values, nulls);
+        result = HeapTupleGetDatum(tuple);
+
+        pfree(values);
+        pfree(nulls);
+
+        SRF_RETURN_NEXT(funcctx, result);
+    } else {
+        SRF_RETURN_DONE(funcctx);
+    }
+}
+
+
+/* Deprecated code starts here
+ * This code is used on v3.5 and under
+ * For consistency with other withPoints functions is kept
+ * but it returns a warning
+ *
+ * TODO(v5) Move to legacy
+ */
+PGDLLEXPORT Datum _pgr_withpointsdd(PG_FUNCTION_ARGS);
+PG_FUNCTION_INFO_V1(_pgr_withpointsdd);
+
+PGDLLEXPORT Datum
+_pgr_withpointsdd(PG_FUNCTION_ARGS) {
+    FuncCallContext     *funcctx;
+    TupleDesc               tuple_desc;
+
+    MST_rt *result_tuples = 0;
+    size_t result_count = 0;
+
+    ereport(WARNING, (
+                errcode(ERRCODE_WARNING_DEPRECATED_FEATURE),
+                errmsg("A stored procedure is using deprecated C internal function '%s'", __func__),
+                errdetail("Library function '%s' was deprecated in pgRouting %s", __func__, "3.6.0"),
+                errhint("Consider upgrade pgRouting")));
+
+    if (SRF_IS_FIRSTCALL()) {
+        MemoryContext   oldcontext;
+        funcctx = SRF_FIRSTCALL_INIT();
+        oldcontext = MemoryContextSwitchTo(funcctx->multi_call_memory_ctx);
+
+        process(
+                text_to_cstring(PG_GETARG_TEXT_P(0)),
+                text_to_cstring(PG_GETARG_TEXT_P(1)),
+                PG_GETARG_ARRAYTYPE_P(2),
+                PG_GETARG_FLOAT8(3),
+
+                PG_GETARG_BOOL(4),
+                text_to_cstring(PG_GETARG_TEXT_P(5)),
+                PG_GETARG_BOOL(6),
+                PG_GETARG_BOOL(7),
+
+                false,
+                &result_tuples, &result_count);
+
+        funcctx->max_calls = result_count;
+
+        funcctx->user_fctx = result_tuples;
+        if (get_call_result_type(fcinfo, NULL, &tuple_desc) != TYPEFUNC_COMPOSITE) {
+            ereport(ERROR,
+                    (errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+                     errmsg("function returning record called in context "
+                         "that cannot accept type record")));
+        }
+
+        funcctx->tuple_desc = tuple_desc;
+        MemoryContextSwitchTo(oldcontext);
+    }
+    funcctx = SRF_PERCALL_SETUP();
+
+    tuple_desc = funcctx->tuple_desc;
+    result_tuples = (MST_rt*) funcctx->user_fctx;
+
+    if (funcctx->call_cntr < funcctx->max_calls) {
+        HeapTuple    tuple;
+        Datum        result;
+        Datum *values;
+        bool* nulls;
+
+        size_t numb = 6;
+        values = palloc(numb * sizeof(Datum));
+        nulls = palloc(numb * sizeof(bool));
+
+        size_t i;
+        for (i = 0; i < numb; ++i) {
+            nulls[i] = false;
+        }
+
+        values[0] = Int32GetDatum((int32_t)funcctx->call_cntr + 1);
+        values[1] = Int64GetDatum(result_tuples[funcctx->call_cntr].from_v);
+        values[2] = Int64GetDatum(result_tuples[funcctx->call_cntr].node);
+        values[3] = Int64GetDatum(result_tuples[funcctx->call_cntr].edge);
+        values[4] = Float8GetDatum(result_tuples[funcctx->call_cntr].cost);
+        values[5] = Float8GetDatum(result_tuples[funcctx->call_cntr].agg_cost);
 
         tuple = heap_form_tuple(tuple_desc, values, nulls);
         result = HeapTupleGetDatum(tuple);
