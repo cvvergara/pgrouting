945f2cd1d9ac8182bcd0c3d2ecf71920120c91cc
cvvergara <vicky@erosion.dev>
2026-01-19 15:17:43 -0600

    (include) Standardizing GPL-2 license text (.h)

--- include/drivers/topologicalSort/topologicalSort_driver.h
+++ include/drivers/topologicalSort/topologicalSort_driver.h
@@ -3,7 +3,7 @@
 File: topologicalSort_driver.h
 
 Generated with Template by:
-Copyright (c) 2015 pgRouting developers
+Copyright (c) 2007-2026 pgRouting developers
 Mail: project@pgrouting.org
 
 Function's developer:
@@ -26,7 +26,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-********************************************************************PGR-GNU*/
+ ********************************************************************PGR-GNU*/
 
 #ifndef INCLUDE_DRIVERS_TOPOLOGICALSORT_TOPOLOGICALSORT_DRIVER_H_
 #define INCLUDE_DRIVERS_TOPOLOGICALSORT_TOPOLOGICALSORT_DRIVER_H_
