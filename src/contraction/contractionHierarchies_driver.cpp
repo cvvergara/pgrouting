c33b8db2a905335f56e3cf965a7afaf0ff82fc37
cvvergara <vicky@erosion.dev>
2026-09-21 02:07:27 -0600

    not running alg

--- src/contraction/contractionHierarchies_driver.cpp
+++ src/contraction/contractionHierarchies_driver.cpp
@@ -91,15 +91,15 @@ pgr_contractionHierarchies(
             DirectedGraph digraph;
             digraph.insert_edges(edges);
 
-            contractionHierarchies(digraph, directed, forbid, log, err);
-            // graph_to_tuple(digraph, return_tuples, return_count);
+            //contractionHierarchies(digraph, directed, forbid, log, err);
+            graph_to_tuple(digraph, return_tuples, return_count);
         } else {
             using UndirectedGraph = pgrouting::graph::CHUniqueUndirectedGraph;
             UndirectedGraph undigraph;
             undigraph.insert_edges(edges);
 
-            contractionHierarchies(undigraph, directed, forbid, log, err);
-            // graph_to_tuple(undigraph, return_tuples, return_count);
+            //contractionHierarchies(undigraph, directed, forbid, log, err);
+            graph_to_tuple(undigraph, return_tuples, return_count);
         }
 
         pgassert(err.str().empty());
