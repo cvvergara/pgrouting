059c6146950255ad0925eacd8c2af2b053b39f0d
cvvergara <vicky@erosion.dev>
2026-09-21 03:40:22 -0600

    with 14

--- include/contraction/contractionHierarchies.hpp
+++ include/contraction/contractionHierarchies.hpp
@@ -297,7 +297,7 @@ void contractionHierarchies(
             contracted_vertex.second = ordered_vertex.second;
             priority_queue.push(contracted_vertex);
         }
-        if (count++ > 13) break;
+        if (count++ > 14) break;
     }
     log << std::endl << "Copy shortcuts" << std::endl;
     graph.copy_shortcuts(shortcuts, log);
